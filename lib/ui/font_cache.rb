59baf5c312f418aae90344761942b27e7ab24747
Jared Hodgkins <PhusionDev@gmail.com>
2012-03-19 15:09:57 -0400

    added some default values to top for easy modification

--- lib/ui/font_cache.rb
+++ lib/ui/font_cache.rb
@@ -1,19 +1,30 @@
 ﻿class Font_Cache
+  # Default font cache values and population behavior
+  # Edit to fit your project's needs.
+  # Font heights are measured in pixels.
+  Min_Font_Height = 12
+  Max_Font_Height = 48
+  Interval = 2
+  # Default fall-back font to return when an invalid font
+  # is passed to the font cache
+  Font = "Arial"
+  Height = 14
+  
   attr_accessor :fonts
   
   def initialize
     @font_heights = []
     populate_font_heights
     @font_names = ["Arial", "Times New Roman", "Verdana"]
-    @fonts = Hash.new(Gosu::Font.new($output.active_window, "Arial", 14))
+    @fonts = Hash.new(Gosu::Font.new($output.active_window, Font, Height))
     load_fonts
   end
   
   private
   def populate_font_heights
-    min_height = 12
-    max_height = 48
-    by_sequence_of = 2
+    min_height = Min_Font_Height
+    max_height = Max_Font_Height
+    by_sequence_of = Interval
     ((max_height-min_height)/by_sequence_of).times do
       @font_heights.push(min_height)
       min_height += by_sequence_of
@@ -26,7 +37,8 @@
         if @fonts['default'].name == font_name && @fonts['default'].height == font_height
           @fonts.store((font_name + "_" + font_height.to_s).to_sym, @fonts['default'])
         else
-          @fonts.store((font_name + "_" + font_height.to_s).to_sym, Gosu::Font.new($output.active_window, font_name, font_height))
+          @fonts.store((font_name + "_" + font_height.to_s).to_sym,
+                        Gosu::Font.new($output.active_window, font_name, font_height))
         end
       end
     end
