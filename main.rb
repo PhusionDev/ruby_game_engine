24f052e901df184b6cf5f6fb12cbb3fa310addad
Jared Hodgkins <PhusionDev@gmail.com>
2012-03-23 06:55:53 -0400

    modified title bar

--- main.rb
+++ main.rb
@@ -3,7 +3,7 @@
 require_relative 'lib/mixins'
 require_relative 'lib/game_window'
 
-$version = "Untitled Game Engine (Ruby w/Gosu) by PhusionDev"
+$version = "press spacebar to test a Color_Transition game object"
 $application_path = Dir.pwd
 
 $game_window = Game_Window.new
